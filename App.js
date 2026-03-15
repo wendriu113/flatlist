a6168bba4c16e81448aff37c90d9c7fa7962dc1a
wendriu113 <wendriuluiz113@gmail.com>
2026-03-15 11:29:18 -0300

    feat: implement FlatList component, add image assets, and configure project files.

--- App.js
+++ App.js
@@ -2,12 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 
 import { FlatList, StyleSheet, Text, View, Image, TouchableOpacity, Linking, Alert } from 'react-native';
 
-const filmes = [
+const Campeoes = [
+  {
+    id: 0,
+    nome: 'Wendriu',
+    lancamento: '13 de dezembro de 2013',
+    genero: 'Mid/Top',
+    maestria: '240.817',
+    imagem: require('./assets/wendriu.jpg'),
+    sinopse: "Zed é um espadachim do jogo League of Legends. Ele usa vento para lutar, tem muita mobilidade e pode bloquear projéteis com uma parede de vento. É forte quando acerta combos e críticos, mas é difícil de jogar bem.",
+    url: "https://universe.leagueoflegends.com/pt_BR/story/champion/yasuo/"
+  },
   {
     id: 1,
     nome: 'Yasuo',
     lancamento: '13 de dezembro de 2013',
-    genero: 'Ficção',
+    genero: 'Mid/Top',
     maestria: '240.817',
     imagem: require('./assets/yasuo1.jpg'),
     sinopse: "Yasuo é um espadachim do jogo League of Legends. Ele usa vento para lutar, tem muita mobilidade e pode bloquear projéteis com uma parede de vento. É forte quando acerta combos e críticos, mas é difícil de jogar bem.",
@@ -17,7 +27,7 @@ const filmes = [
     id: 2,
     nome: 'Ekko',
     lancamento: '29 de maio de 2015.',
-    genero: 'Ficção',
+    genero: 'Jungle/Mid',
     maestria: '230.142',
     imagem: require('./assets/ekko.jpg'),
     sinopse: "Ekko é um jovem inventor de Zaun no jogo League of Legends. Ele usa tecnologia para manipular o tempo, podendo voltar alguns segundos para evitar morrer ou refazer uma jogada. É rápido e causa bastante dano quando acerta o combo.",
@@ -27,78 +37,82 @@ const filmes = [
     id: 3,
     nome: 'Swain',
     lancamento: '5 de outubro de 2010.',
-    genero: 'Ficção',
+    genero: 'Mid/Support',
     maestria: '154.081',
-    imagem: require('./assets/ekko.jpg'),
+    imagem: require('./assets/swain.jpg'),
     sinopse: "Swain é um poderoso líder de Noxus no jogo League of Legends. Ele usa magia sombria e corvos para drenar vida dos inimigos e ficar mais forte nas lutas.",
-    url: "https://universe.leagueoflegends.com/pt_BR/champion/ekko/"
+    url: "https://universe.leagueoflegends.com/pt_BR/champion/swain/"
   },
   {
     id: 4,
-    nome: 'Ekko',
-    lancamento: '29 de maio de 2015.',
-    genero: 'Ficção',
-    maestria: '230.142',
-    imagem: require('./assets/ekko.jpg'),
-    sinopse: "Ekko é um jovem inventor de Zaun no jogo League of Legends. Ele usa tecnologia para manipular o tempo, podendo voltar alguns segundos para evitar morrer ou refazer uma jogada. É rápido e causa bastante dano quando acerta o combo.",
-    url: "https://universe.leagueoflegends.com/pt_BR/champion/ekko/"
+    nome: 'Samira',
+    lancamento: '21 de setembro de 2020.',
+    genero: 'ADC',
+    maestria: '122.917',
+    imagem: require('./assets/Samira.jpg'),
+    sinopse: "Samira é uma atiradora extremamente agressiva e estilosa, que usa pistolas e espada para lutar. Ela gosta de combates arriscados e fica mais forte conforme encadeia habilidades diferentes para aumentar seu estilo de luta.",
+    url: "https://universe.leagueoflegends.com/pt_BR/champion/samira/"
   },
   {
     id: 5,
-    nome: 'Ekko',
-    lancamento: '29 de maio de 2015.',
-    genero: 'Ficção',
-    maestria: '230.142',
-    imagem: require('./assets/ekko.jpg'),
-    sinopse: "Ekko é um jovem inventor de Zaun no jogo League of Legends. Ele usa tecnologia para manipular o tempo, podendo voltar alguns segundos para evitar morrer ou refazer uma jogada. É rápido e causa bastante dano quando acerta o combo.",
-    url: "https://universe.leagueoflegends.com/pt_BR/champion/ekko/"
+    nome: 'Rengar',
+    lancamento: '21 de agosto de 2012.',
+    genero: 'Jungle/Top',
+    maestria: '101.427',
+    imagem: require('./assets/rengar.jpg'),
+    sinopse: "Rengar é um caçador feroz da selva, parecido com um leão, que adora caçar inimigos fortes para provar sua força. Ele usa furtividade e saltos rápidos para eliminar os adversários.",
+    url: "https://universe.leagueoflegends.com/pt_BR/champion/rengar/"
   },
   {
     id: 6,
-    nome: 'Ekko',
-    lancamento: '29 de maio de 2015.',
-    genero: 'Ficção',
-    maestria: '230.142',
-    imagem: require('./assets/ekko.jpg'),
-    sinopse: "Ekko é um jovem inventor de Zaun no jogo League of Legends. Ele usa tecnologia para manipular o tempo, podendo voltar alguns segundos para evitar morrer ou refazer uma jogada. É rápido e causa bastante dano quando acerta o combo.",
-    url: "https://universe.leagueoflegends.com/pt_BR/champion/ekko/"
+    nome: 'Udyr',
+    lancamento: '2 de dezembro de 2009.',
+    genero: 'Jungle',
+    maestria: '97.477',
+    imagem: require('./assets/udyr.jpg'),
+    sinopse: "Udyr é um guerreiro espiritual que luta usando posturas de animais, ganhando habilidades diferentes como força, velocidade e resistência.",
+    url: "https://universe.leagueoflegends.com/pt_BR/champion/udyr/"
   }
 ];
 
 export default function App() {
 
-  const criaItem = ({ item }) => (
-    <TouchableOpacity 
-      style={styles.listaItem} 
-      onPress={() => 
-        Alert.alert(
-          'Lore', 
-          item.sinopse, 
-          [
-            {
-              text: 'Ler Lore', 
-              onPress: () => Linking.openURL(item.url), 
-            },
-            {
-              text: 'Fechar', 
-              style: 'cancel', 
-            }
-          ]
-        )
-      }
-    >
-      <Image source={item.imagem} style={styles.listaImagem} />
+  const criaItem = ({ item }) => {
+    const isWendriu = item.nome === 'Wendriu';
 
-      <View style={styles.listaDetalhes}>
-        
-        <Text style={styles.textoForte}>Campeão: <Text style={styles.textoNormal}>{item.nome}</Text></Text>
-        <Text style={styles.textoForte}>lancamento: <Text style={styles.textoNormal}>{item.lancamento}</Text></Text>
-        <Text style={styles.textoForte}>Gênero: <Text style={styles.textoNormal}>{item.genero}</Text></Text>
-        <Text style={styles.textoForte}>Maestria: <Text style={styles.textoNormal}>{item.maestria}</Text></Text>     
-      </View>
+    return (
+      <TouchableOpacity 
+        style={styles.listaItem} 
+        onPress={() => 
+          Alert.alert(
+            'Lore', 
+            item.sinopse, 
+            [
+              {
+                text: 'Ler Lore', 
+                onPress: () => Linking.openURL(item.url), 
+              },
+              {
+                text: 'Fechar', 
+                style: 'cancel', 
+              }
+            ]
+          )
+        }
+      >
+        <Image source={item.imagem} style={styles.listaImagem} />
 
-    </TouchableOpacity>
-  );
+        <View style={styles.listaDetalhes}>
+          
+          <Text style={styles.textoForte}>{isWendriu ? 'Main atual: ' : 'Campeão: '}<Text style={styles.textoNormal}>{item.nome}</Text></Text>
+          <Text style={styles.textoForte}>{isWendriu ? 'Joga desde: ' : 'Lançamento: '}<Text style={styles.textoNormal}>{item.lancamento}</Text></Text>
+          <Text style={styles.textoForte}>{isWendriu ? 'Rotas preferidas: ' : 'Rota: '}<Text style={styles.textoNormal}>{item.genero}</Text></Text>
+          <Text style={styles.textoForte}>Maestria: <Text style={styles.textoNormal}>{item.maestria}</Text></Text>     
+        </View>
+
+      </TouchableOpacity>
+    );
+  };
 
   return (
     <View style={styles.container}>
@@ -106,7 +120,7 @@ export default function App() {
       <Text style={styles.titulo}>Mains LOL</Text>
 
       <FlatList 
-        data={filmes} 
+        data={Campeoes} 
         renderItem={criaItem} 
         keyExtractor={item => item.id.toString()} 
         showsVerticalScrollIndicator={false} 
